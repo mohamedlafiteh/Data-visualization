af0a4edcac1977f2bed430c2da76b6ef018ab4e7
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-05-14 13:16:08 +0100

    First commit add the app files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {  VictoryChart,VictoryVoronoiContainer,
+  VictoryTooltip,VictoryLine} from 'victory';
+import data from './media-data/jstest.json'
+import Navbar from "./components/Navbar"
+import './App.css'
+class App extends React.Component {
+  state ={
+    mediaData:data
+  }
+  render() {
+    const {mediaData} = this.state
+    return (
+      <div>
+         <Navbar/>
+     
+      <div className="container">
+     
+      <div className="child">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <VictoryChart height={400} width={400}
+        domainPadding={{ y: 10 }}
+        containerComponent={
+          <VictoryVoronoiContainer
+            voronoiDimension="x"
+            labels={({ datum }) => `y: ${datum.y}`}
+            labelComponent={
+              <VictoryTooltip
+                cornerRadius={0}
+                flyoutStyle={{ fill: "white" }}
+              />}
+          />}
+      >
+        <VictoryLine
+            data = {mediaData.DM.map((number,index) =>{
+              let dataObj={ x:index , y: number}
+              return dataObj
+            })}
+          style={{
+            data: {
+              stroke: "tomato",
+              strokeWidth: ({ active }) => active ? 4 : 2
+            },
+            labels: { fill: "tomato" }
+          }}
+        />
+
+        <VictoryLine
+            data = {mediaData.TV.map((number,index) =>{
+              let dataObj={ x:index , y: number}
+              return dataObj
+            })}
+          style={{
+            data: {
+              stroke: "blue",
+              strokeWidth: ({ active }) => active ? 4 : 2
+            },
+            labels: { fill: "blue" }
+          }}
+        />
+
+        <VictoryLine
+            data = {mediaData.OOH.map((number,index) =>{
+              let dataObj={ x:index , y: number}
+              return dataObj
+            })}
+          style={{
+            data: {
+              stroke: "green",
+              strokeWidth: ({ active }) => active ? 4 : 2
+            },
+            labels: { fill: "green" }
+          }}
+        />
+         <VictoryLine
+        
+            data = {mediaData.PPC.map((number,index) =>{
+              let dataObj={ x:index , y: number}
+              return dataObj
+            })}
+          style={{
+            data: {
+              stroke: "black",
+              strokeWidth: ({ active }) => active ? 4 : 2
+            },
+            labels: { fill: "black" }
+          }}
+        />
+      </VictoryChart>
+       </div>
+      </div>
+       </div>
+    );
+  }
 }
 
 export default App;
