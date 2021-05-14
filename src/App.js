49f2af89514891138c78d40bf6a3b0fdac68a9b6
mohamedlafiteh <m_lafiteh@yahoo.com>
2021-05-14 15:04:44 +0100

    Add navbar and fix the style to center

--- src/App.js
+++ src/App.js
@@ -13,82 +13,79 @@ class App extends React.Component {
     return (
       <div>
          <Navbar/>
-     
-      <div className="container">
-     
-      <div className="child">
+        <div className="container">
+            <div className="child">
+            <VictoryChart height={400} width={400}
+              domainPadding={{ y: 10 }}
+              containerComponent={
+                <VictoryVoronoiContainer
+                  voronoiDimension="x"
+                  labels={({ datum }) => `${datum.a}: ${datum.y}`}
+                  labelComponent={
+                    <VictoryTooltip
+                      cornerRadius={0}
+                      flyoutStyle={{ fill: "black" }}
+                    />}
+                />}
+            >
+              <VictoryLine
+                  data = {mediaData.DM.map((number,index) =>{
+                    let dataObj={ x:index , y: number,a:"DM"}
+                    return dataObj
+                  })}
+                style={{
+                  data: {
+                    stroke: "tomato",
+                    strokeWidth: ({ active }) => active ? 4 : 2
+                  },
+                  labels: { fill: "tomato" },
+                }}
+              />
 
-      <VictoryChart height={400} width={400}
-        domainPadding={{ y: 10 }}
-        containerComponent={
-          <VictoryVoronoiContainer
-            voronoiDimension="x"
-            labels={({ datum }) => `y: ${datum.y}`}
-            labelComponent={
-              <VictoryTooltip
-                cornerRadius={0}
-                flyoutStyle={{ fill: "white" }}
-              />}
-          />}
-      >
-        <VictoryLine
-            data = {mediaData.DM.map((number,index) =>{
-              let dataObj={ x:index , y: number}
-              return dataObj
-            })}
-          style={{
-            data: {
-              stroke: "tomato",
-              strokeWidth: ({ active }) => active ? 4 : 2
-            },
-            labels: { fill: "tomato" }
-          }}
-        />
+              <VictoryLine
+                  data = {mediaData.TV.map((number,index) =>{
+                    let dataObj={ x:index , y: number,number,a:"TV"}
+                    return dataObj
+                  })}
+                style={{
+                  data: {
+                    stroke: "blue",
+                    strokeWidth: ({ active }) => active ? 4 : 2
+                  },
+                  labels: { fill: "blue" }
+                }}
+              />
 
-        <VictoryLine
-            data = {mediaData.TV.map((number,index) =>{
-              let dataObj={ x:index , y: number}
-              return dataObj
-            })}
-          style={{
-            data: {
-              stroke: "blue",
-              strokeWidth: ({ active }) => active ? 4 : 2
-            },
-            labels: { fill: "blue" }
-          }}
-        />
-
-        <VictoryLine
-            data = {mediaData.OOH.map((number,index) =>{
-              let dataObj={ x:index , y: number}
-              return dataObj
-            })}
-          style={{
-            data: {
-              stroke: "green",
-              strokeWidth: ({ active }) => active ? 4 : 2
-            },
-            labels: { fill: "green" }
-          }}
-        />
-         <VictoryLine
-        
-            data = {mediaData.PPC.map((number,index) =>{
-              let dataObj={ x:index , y: number}
-              return dataObj
-            })}
-          style={{
-            data: {
-              stroke: "black",
-              strokeWidth: ({ active }) => active ? 4 : 2
-            },
-            labels: { fill: "black" }
-          }}
-        />
-      </VictoryChart>
-       </div>
-      </div>
+              <VictoryLine
+                  data = {mediaData.OOH.map((number,index) =>{
+                    let dataObj={ x:index , y: number,a:"OOH"}
+                    return dataObj
+                  })}
+                style={{
+                  data: {
+                    stroke: "green",
+                    strokeWidth: ({ active }) => active ? 4 : 2
+                  },
+                  labels: { fill: "green" }
+                }}
+              />
+              <VictoryLine
+              
+                  data = {mediaData.PPC.map((number,index) =>{
+                    let dataObj={ x:index , y: number,a:"PPC"}
+                    return dataObj
+                  })}
+                style={{
+                  data: {
+                    stroke: "black",
+                    strokeWidth: ({ active }) => active ? 4 : 2
+                  },
+                  labels: { fill: "black" }
+                }}
+              />
+            </VictoryChart>
+            </div>
+        </div>
        </div>
     );
   }
